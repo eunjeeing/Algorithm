7199068d489fa01e15a53b3b14484fbc3d6949eb
eunjeeing <ejjeongggg@gmail.com>
2024-04-06 07:25:58 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,18 +1,26 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 		int t = Integer.parseInt(br.readLine());
-		String answer = "";
+		br.close();
 		
 		for(int i = 1; i <= t; i++) {
-			answer += "*";
-			System.out.println(answer);
+			for(int j = 1; j<= i; j++) {
+				bw.write("*");
+			}
+			bw.newLine();
 		}
+		
+		bw.flush();
+		bw.close();
 	}
 }
