3c9620bfaa3d5e6041114a52efda71d7311408c5
eunjeeing <ejjeongggg@gmail.com>
2024-04-08 19:56:58 +0900

    [Silver V] Title: 너의 평점은, Time: 136 ms, Memory: 14596 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		HashMap<String, Double> map = new HashMap<>();
+		
+		double sum = 0.0;
+		double sumScore = 0.0;
+			
+		map.put("A+", 4.5);
+		map.put("A0", 4.0);
+		map.put("B+", 3.5);
+		map.put("B0", 3.0);
+		map.put("C+", 2.5);
+		map.put("C0", 2.0);
+		map.put("D+", 1.5);
+		map.put("D0", 1.0);
+		map.put("F", 0.0);
+
+		for(int i = 0; i < 20; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			String name = st.nextToken();
+			double score = Double.parseDouble(st.nextToken());
+			String grade = st.nextToken();
+			
+			if(!grade.equals("P")) {
+				sum += map.get(grade)*score;
+				sumScore += score;
+			}
+		}
+		System.out.printf("%.6f\n", sum/sumScore);
+		
+	}
+}
