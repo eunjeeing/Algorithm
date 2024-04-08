1c568066c4fbaeca18f7f4f4e16223328d598846
eunjeeing <ejjeongggg@gmail.com>
2024-04-09 00:23:56 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 228 ms, Memory: 18376 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -13,7 +13,6 @@ public class Main {
 		int M = Integer.parseInt(st.nextToken());
 		
 		int[][] arr1 = new int[N][M];
-		int[][] arr2 = new int[N][M];
 		
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -21,16 +20,15 @@ public class Main {
 				arr1[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < M; j++) {
-				arr2[i][j] = Integer.parseInt(st.nextToken());
-				System.out.print(arr1[i][j] + arr2[i][j] + " ");
+				sb.append(arr1[i][j] + Integer.parseInt(st.nextToken()) + " ");
 			}
-			System.out.println();
+			sb.append("\n");
 		}
-			
+		System.out.println(sb);
 	}
 
 }
