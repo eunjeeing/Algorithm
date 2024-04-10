1e3526843585bc55d92940e05add4156e80730a9
eunjeeing <ejjeongggg@gmail.com>
2024-04-10 20:07:20 +0900

    [Bronze I] Title: 진법 변환 2, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long N = Long.parseLong(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		br.close();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while(N > 0) {
+			if(N % B < 10) {
+				sb.append(N % B);
+			} else {
+				sb.append((char)(N % B - 10 + 'A'));
+			}
+			N /= B;
+		}
+		System.out.println(sb.reverse());
+		
+	}
+}
