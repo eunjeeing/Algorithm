9fe4a7742a83d6c5a583b3d36a57054c12bac4e7
eunjeeing <ejjeongggg@gmail.com>
2024-04-13 09:42:43 +0900

    [Bronze II] Title: 분해합, Time: 1496 ms, Memory: 308240 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int result = 0;
+
+		for (int i = 1; i <= N; i++) {
+
+			String num = i + "";
+			String[] numArr = num.split("");
+			int sum = 0;
+			for (int j = 0; j < numArr.length; j++) {
+				sum += Integer.parseInt(numArr[j]);
+			}
+			if (sum + Integer.parseInt(num) == N) {
+				result = Integer.parseInt(num);
+				System.out.println(result);
+					break;
+			}
+
+		}
+		if (result == 0) {
+			System.out.println(0);
+
+		}
+
+	}
+
+}
