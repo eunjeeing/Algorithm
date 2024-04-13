df90aa60acf9a68460ccba2a7e7363f3eb6626a3
eunjeeing <ejjeongggg@gmail.com>
2024-04-13 11:22:32 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 140 ms, Memory: 15848 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,31 +1,30 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-		int d = Integer.parseInt(st.nextToken());
-		int e = Integer.parseInt(st.nextToken());
-		int f = Integer.parseInt(st.nextToken());
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         br.close();
-		
-		for(int i = -999; i < 1000; i++) {
-			for(int j = -999; j < 1000; j++) {
-				if((a * i + b * j == c) && (d * i + e * j == f)) {
-					System.out.println(i + " " + j);
-                    break;
-				}
-			}
-		}
-		
-		
-	}
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        bw.write((c*e - b*f) / (a*e - b*d) + " "); // x
+        bw.write((c*d - a*f) / (b*d - a*e)+ ""); // y
+        bw.flush();
+        bw.close();
+
+    }
 
 }
