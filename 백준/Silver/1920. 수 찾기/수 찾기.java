a419cf34afd1165545a786fef4bbdff8f31cf31f
eunjeeing <ejjeongggg@gmail.com>
2024-04-15 00:09:46 +0900

    [Silver IV] Title: 수 찾기, Time: 600 ms, Memory: 51136 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		HashSet<Integer> set = new HashSet<>();
+		
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			set.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		int M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			if(set.contains(num)) sb.append(1).append("\n");
+			else sb.append(0).append("\n");
+		}
+		br.close();
+		System.out.println(sb);
+	}
+
+}
