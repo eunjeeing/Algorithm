dae770ed9e560fdb82fa9b416e9097cafec65980
eunjeeing <ejjeongggg@gmail.com>
2024-04-28 20:22:19 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -8,23 +8,18 @@ public class Main {
 	public static int M,N;
 	public static int result = 0;
 	
-	public static int bfs(int start) {
-		Queue<Integer> q = new LinkedList<>();
+	public static int dfs(int start) {
 		
-		q.offer(start);
 		visited[start] = true;
 		
-		while(!q.isEmpty()) {
-			int temp = q.poll();
-			
-			for(int i = 1; i <= M; i++) {				
-				if(node[temp][i] == 1 && !visited[i]) {
-					visited[i] = true;
-					q.offer(i);
-					result++;
-				}
+		for(int i = 1; i <= M; i++) {
+			if(node[start][i] == 1 && !visited[i]) {
+				dfs(i);
+				result++;
 			}
+
 		}
+		
 		return result;
 	}
 	
@@ -32,9 +27,10 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		M = Integer.parseInt(br.readLine());
 		N = Integer.parseInt(br.readLine());
+		
 		node = new int[M+1][M+1];
 		visited = new boolean[M+1];
-		Queue<Integer> queue = new LinkedList<>();
+		
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int m = Integer.parseInt(st.nextToken());
@@ -42,7 +38,7 @@ public class Main {
 			node[m][n] = node[n][m] = 1;
 		}
 		
-		System.out.println(bfs(1));
+		System.out.println(dfs(1));
 	}
 
 }
