5b76e6b8655614bc2a289af19f24870209fe5e67
eunjeeing <ejjeongggg@gmail.com>
2024-05-13 20:15:06 +0900

    [Silver III] Title: N과 M (7), Time: 824 ms, Memory: 265464 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int N, M;
+	static boolean[] check;
+	static int[] arr, arr2;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		check = new boolean[N+1];
+		arr = new int[N+1];
+		arr2 = new int[N+1];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		bfs(0, 0);
+		
+		System.out.println(sb);
+        br.close();
+	}
+	
+	static void bfs (int idx, int depth) {
+		if (depth == M) {
+			for (int i = 1; i < depth + 1; i++) {
+				sb.append(arr2[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = 1; i < N + 1; i++) {
+				arr2[depth+1] = arr[i];
+				
+				bfs(i+1, depth+1);
+		}
+	}
+	
+}
