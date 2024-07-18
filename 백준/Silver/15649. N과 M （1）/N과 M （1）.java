228ecd65e254271ba913e5e016a30e7194c5f0d8
eunjeeing <ejjeongggg@gmail.com>
2024-07-18 21:14:57 +0900

    [Silver III] Title: N과 M (1), Time: 308 ms, Memory: 33764 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -2,47 +2,46 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int N, M;
-	static boolean[] check;
-	static int[] arr;
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws Exception {
-	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		check = new boolean[N+1];
-		arr = new int[N+1];
-		for(int i = 1; i < N+1; i++) {
-			arr[i] = i;
-		}
-		
-		backTracking(0);
-		System.out.println(sb);
-        br.close();
-	}
-	
-	static void backTracking (int K) {
-		if (K == M) {
-			for (int i = 1; i < K + 1; i++) {
-				sb.append(arr[i] + " ");
-			}
-			sb.append("\n");
-			return;
-		}
-		
-		for (int i = 1; i < N + 1; i++) {
-			if(!check[i]) {
-				check[i] = true;
-				arr[K+1] = i;
-				backTracking(K+1);
-				check[i] = false;
-			}
-		}
-	}
-	
+
+    static int N, M;
+    static int[] arr;
+    static boolean[] check;
+    static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        check = new boolean[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = i;
+        }
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int k) {
+        if (k == M) {
+            for (int i = 0; i < k; i++) {
+                sb.append(arr[i]+1 + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if(!check[i]) {
+                check[i] = true;
+                arr[k] = i;
+                dfs(k+1);
+                check[i] = false;
+            }
+        }
+    }
 }
