1db685f6fa4e34d06cfc81479a2de175f7c7d871
eunjeeing <ejjeongggg@gmail.com>
2024-09-28 17:22:25 +0900

    [level 3] Title: 네트워크, Time: 0.51 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,33 +1,33 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     
     static boolean[] visited;
     
     public int solution(int n, int[][] computers) {
-        
+        int answer = 0;
         visited = new boolean[n];
-        int count = 0;
         
         for (int i = 0; i < n; i++) {
             if(!visited[i]) {
                 bfs(n, computers, i);
-               count++;
+                answer++;
             }
         }
-           
-        return count;
+        
+        return answer;
     }
     
-    public void bfs(int n, int[][] computers, int start) {
+    static void bfs(int n, int[][] computers, int start) {
         Queue<Integer> q = new LinkedList<>();
         q.offer(start);
         visited[start] = true;
         
         while(!q.isEmpty()) {
-            int temp = q.poll();
+            int now = q.poll();
             for (int i = 0; i < n; i++) {
-                if (computers[temp][i] == 1 && !visited[i]) {
+                if (computers[now][i] == 1 && !visited[i]) {
                     q.offer(i);
                     visited[i] = true;
                 }
