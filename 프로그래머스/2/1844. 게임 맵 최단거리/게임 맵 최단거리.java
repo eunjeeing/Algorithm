a58d58d37deb8e1feb2abce8f8925d9408ee7570
eunjeeing <ejjeongggg@gmail.com>
2024-10-03 17:24:01 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.74 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,50 +3,52 @@ import java.io.*;
 
 class Solution {
     
-    static boolean[][] visited;
     static int n, m;
+    static boolean[][] visited;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
     
     public int solution(int[][] maps) {
-
+        int answer = 0;
+        
         n = maps.length;
         m = maps[0].length;
+        
         visited = new boolean[n][m];
         
-        // List<Integer> list = new ArrayList<>();
-        // int result = bfs(maps, 0, 0);
-        // list.add(result);
-            
-        // Collections.sort(list);
-        
-        int answer = bfs(maps, 0, 0);
-        return answer;
+        return bfs(maps, new Point(0, 0));
     }
     
-    public static int bfs(int[][] maps, int x, int y) {
-        Queue<int[]> q = new LinkedList<>();
-        visited[x][y] = true;
-        q.offer(new int[] {x, y});
+    static int bfs(int[][] maps, Point start) {
+        Queue<Point> q = new LinkedList<>();
+        q.offer(start);
+        visited[start.x][start.y] = true;
         
         while(!q.isEmpty()) {
-            int[] now = q.poll();
-            int nowX = now[0];
-            int nowY = now[1];
+            Point current = q.poll();
             
             for (int i = 0; i < 4; i++) {
-                int nextX = nowX + dx[i];
-                int nextY = nowY + dy[i];
+                int nx = current.x + dx[i];
+                int ny = current.y + dy[i];
                 
-                if (nextX >= 0 && nextX < n && nextY >= 0 && nextY < m) {
-                    if (!visited[nextX][nextY] && maps[nextX][nextY] == 1) {
-                        q.offer(new int[] {nextX, nextY});
-                        visited[nextX][nextY] = true;
-                        maps[nextX][nextY] = maps[nowX][nowY] + 1;
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+                    if (maps[nx][ny] == 1 && !visited[nx][ny]) {
+                        q.offer(new Point(nx, ny));
+                        visited[nx][ny] = true;
+                        maps[nx][ny] = maps[current.x][current.y] + 1;
                     }
                 }
             }
         }
-        return maps[n - 1][m - 1] > 1 ? maps[n - 1][m - 1] : -1;
+        return maps[n-1][m-1] > 1 ? maps[n-1][m-1] : -1;
+    }
+    
+    static class Point {
+        int x, y;
+        
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
