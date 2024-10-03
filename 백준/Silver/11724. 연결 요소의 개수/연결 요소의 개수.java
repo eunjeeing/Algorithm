247534b8640676b1d7cd83b595defdd0bbfdc52c
eunjeeing <ejjeongggg@gmail.com>
2024-10-03 20:19:07 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 556 ms, Memory: 142148 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,50 +2,57 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int N, M, count;
-	static boolean[] visited;
-	static int[][] node;
-	
-	static void bfs(int start) {
-		Queue<Integer> q = new LinkedList<>();
-		q.offer(start);
-		visited[start] = true;
-		
-		while(!q.isEmpty()) {
-			int temp = q.poll();
-			for (int i = 1; i <= N; i++) {
-				if(node[temp][i] == 1 && !visited[i]) {
-					q.offer(i);
-					visited[i] = true;
-				}
-			}
-		}
-		
-	}
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		node = new int[N+1][N+1];
-		visited = new boolean[N+1];
-				
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			
-			node[u][v] = node[v][u] = 1;
-		}
-		
-		for(int i = 1; i <= N; i++) {
-			if(!visited[i]) {
-				bfs(i);
-				count++;
-			}
-		}
-		System.out.println(count);
-	}
 
+    static int N, M;
+    static List<List<Integer>> graph = new ArrayList<>();
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N+1];
+
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        int count = 0;
+        for (int i = 1; i <= N; i++) {
+            if(!visited[i]) {
+                bfs(i);
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        visited[start] = true;
+
+        while(!q.isEmpty()) {
+            int current = q.poll();
+
+            for (int i : graph.get(current)) {
+                if (!visited[i]) {
+                    q.offer(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
 }
