9258e619418d28c6156e919c734ab4525f7b5951
eunjeeing <ejjeongggg@gmail.com>
2024-10-03 16:56:45 +0900

    [Silver I] Title: 안전 영역, Time: 272 ms, Memory: 55184 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -2,66 +2,80 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int N;
-	static int[] dx = { -1, 1, 0, 0 };
-	static int[] dy = { 0, 0, -1, 1 };
-	static boolean[][] visited;
-	static int[][] map;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		int max = 1;
-		map = new int[N][N];
-		StringTokenizer st;
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				max = Math.max(max, map[i][j]);
-			}
-		}
-		
-		int result = 0;
-		for(int i = 0; i <= max; i++) {
-			visited = new boolean[N][N];
-			int cnt = 0;
-			for (int j = 0; j < N; j++) {
-				for (int k = 0; k < N; k++) {
-					if(map[j][k] > i && !visited[j][k]) {
-						cnt++;
-						bfs(j, k, i);
-					}
-				}
-			}
-			result = Math.max(result, cnt);
-		}
-		System.out.println(result);
-		
-	}
-	
-	public static void bfs(int x, int y, int height) {
-		Queue<int[]> q = new LinkedList<>();
-		q.offer(new int[] {x, y});
-		visited[x][y] = true;
-		
-		while(!q.isEmpty()) {
-			int[] now = q.poll();
-			int nowX = now[0];
-			int nowY = now[1];
-			
-			for (int i = 0; i < 4; i++) {
-				int nextX = nowX + dx[i];
-				int nextY = nowY + dy[i];
-				
-				if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < N) {
-					if(!visited[nextX][nextY] && map[nextX][nextY] > height) {
-						q.offer(new int[] {nextX, nextY});
-						visited[nextX][nextY] = true;
-					}
-				}
-			}
-		}
-	}
-	
+
+    static int N;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+
+        map = new int[N][N];
+
+        int max = 1;
+        int min = 100;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                max = Math.max(max, map[i][j]);
+                min = Math.min(min, map[i][j]);
+            }
+        }
+
+        int maxCount = 1;
+        for (int i = min; i <= max; i++) {
+            int count = 0;
+            visited = new boolean[N][N];
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < N; k++) {
+                    if (map[j][k] > i && !visited[j][k]) {
+                        bfs(new Point(j, k), i);
+                        count++;
+                    }
+                }
+            }
+            maxCount = Math.max(count, maxCount);
+        }
+
+        System.out.println(maxCount);
+    }
+
+    static void bfs(Point start, int height) {
+
+        Queue<Point> q = new LinkedList<>();
+        q.offer(start);
+        visited[start.x][start.y] = true;
+
+        while(!q.isEmpty()) {
+            Point current = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = current.x + dx[i];
+                int ny = current.y + dy[i];
+
+                if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+                    if (map[nx][ny] > height && !visited[nx][ny]) {
+                        q.offer(new Point(nx, ny));
+                        visited[nx][ny] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    static class Point {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
