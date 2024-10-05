7f2cd181e277724701b9ab5cbacc0333ef354b9f
eunjeeing <ejjeongggg@gmail.com>
2024-10-05 16:37:04 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,19 +1,22 @@
-SELECT HE.EMP_NO, 
-        HE.EMP_NAME, 
-        CASE 
-            WHEN AVG(HG.SCORE) >= 96 THEN 'S'
-            WHEN AVG(HG.SCORE) >= 90 THEN 'A'
-            WHEN AVG(HG.SCORE) >= 80 THEN 'B'
-            ELSE 'C' 
-        END GRADE,
-        CASE 
-            WHEN AVG(HG.SCORE) >= 96 THEN HE.SAL*0.2
-            WHEN AVG(HG.SCORE) >= 90 THEN HE.SAL*0.15
-            WHEN AVG(HG.SCORE) >= 80 THEN HE.SAL*0.10
-            ELSE HE.SAL*0
-        END BONUS
+WITH CTE AS (
+    SELECT 
+       EMP_NO,
+       CASE
+           WHEN AVG(SCORE) >= 96 THEN 'S'
+           WHEN AVG(SCORE) >= 90 THEN 'A'
+           WHEN AVG(SCORE) >= 80 THEN 'B'
+           ELSE 'C'
+       END AS 'GRADE',
+       CASE
+           WHEN AVG(SCORE) >= 96 THEN 0.2
+           WHEN AVG(SCORE) >= 90 THEN 0.15
+           WHEN AVG(SCORE) >= 80 THEN 0.1
+           ELSE 0
+       END AS 'BONUS'
+FROM HR_GRADE
+GROUP BY EMP_NO)
+SELECT HE.EMP_NO, HE.EMP_NAME, C.GRADE, HE.SAL*C.BONUS AS BONUS
 FROM HR_EMPLOYEES HE
-INNER JOIN HR_GRADE HG
-ON HG.EMP_NO = HE.EMP_NO
-GROUP BY 1
+INNER JOIN CTE C
+ON HE.EMP_NO = C.EMP_NO
 ORDER BY 1
