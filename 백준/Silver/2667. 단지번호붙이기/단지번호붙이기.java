69b103ef83d4225e7c402537b1d896b98c5e3c1b
eunjeeing <ejjeongggg@gmail.com>
2024-10-05 21:57:54 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,75 +2,93 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	
-	static int N, count;
-	static boolean[][] visited;
-	static int[][] map;
-	static int[] dx = { -1, 1, 0, 0 };
-	static int[] dy = { 0, 0, -1, 1 };
-	static List<Integer> list;
-	static StringBuilder sb;
-	
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
-		visited = new boolean[N][N];
-		list = new ArrayList<>();
-		sb = new StringBuilder();
-		
-		for (int i = 0; i < N; i++) {
-			String line = br.readLine();
-			for (int j = 0; j < N; j++) {
-				map[i][j] = line.charAt(j) - '0';
-			}
-		}
-		
-		for (int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				if(map[i][j] == 1 && !visited[i][j]) {
-					count = 0;
-					bfs(i, j);
-					list.add(count);
-				}
-			}
-		}
-		
-		Collections.sort(list);
-		sb.append(list.size()).append("\n");
-		
-		for(int i : list) {
-			sb.append(i).append("\n");
-		}
-		System.out.println(sb);
-		
-	}
-	
-	public static void bfs(int x, int y) {
-		Queue<int[]> q = new LinkedList<>();
-		q.offer(new int[] {x, y});
-		visited[x][y] = true;
-		
-		while(!q.isEmpty()) {
-			int[] now = q.poll();
-			int nowX = now[0];
-			int nowY = now[1];
-			count++;
-			
-			for (int i = 0; i < 4; i++) {
-				int nextX = nowX + dx[i];
-				int nextY = nowY + dy[i];
-				
-				// map의 범위를 벗어난다면 
-				if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= N) continue;
-				if (visited[nextX][nextY] || map[nextX][nextY] == 0) continue;
-				
-				q.offer(new int[] {nextX, nextY});
-				visited[nextX][nextY] = true;
-			}
-		}
-	}
 
+    static int N;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static List<Integer> list = new ArrayList<>();
+//    static int count = 0;
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        map = new int[N][N];
+        visited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if(!visited[i][j] && map[i][j] == 1) {
+//                    int count = bfs(new Point(i, j));
+                    int count = dfs(new Point(i, j));
+                    list.add(count);
+                }
+            }
+        }
+
+        System.out.println(list.size());
+        Collections.sort(list);
+        for (int i : list) {
+            System.out.println(i);
+        }
+    }
+
+    static int dfs(Point start) {
+        visited[start.x][start.y] = true;
+        int count = 1;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = start.x + dx[i];
+            int ny = start.y + dy[i];
+
+            if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+                if (!visited[nx][ny] && map[nx][ny] == 1) {
+                    count += dfs(new Point(nx, ny));
+                }
+            }
+        }
+        return count;
+    }
+
+    static int bfs(Point start) {
+        Queue<Point> q = new LinkedList<>();
+        q.offer(start);
+        visited[start.x][start.y] = true;
+        int count = 1;
+
+        while(!q.isEmpty()) {
+            Point current = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = current.x + dx[i];
+                int ny = current.y + dy[i];
+
+                if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+                    if (!visited[nx][ny] && map[nx][ny] == 1) {
+                        q.offer(new Point(nx, ny));
+                        visited[nx][ny] = true;
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    static class Point {
+        int x, y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
