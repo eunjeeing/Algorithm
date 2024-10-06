e898874646431787d087f0a65db65c35dd3a73e3
eunjeeing <ejjeongggg@gmail.com>
2024-10-06 10:56:06 +0900

    [Silver I] Title: 숨바꼭질, Time: 140 ms, Memory: 18840 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,53 +1,48 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	
-	static int[] dx = { -1, 1, 2 };
-	static int N, K;
-	static int[] chk = new int[100001];
-	
-	public static void main(String[] arg) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		if (N == K) {
-			System.out.println(0);
-		} else {
-			bfs(N);
-		}
-	}
-	
-	static void bfs(int num) {
-		Queue<Integer> q = new LinkedList<>();
-		q.offer(num);
-		chk[num] = 1;
-		
-		while(!q.isEmpty()) {
-			int temp = q.poll();
-			
-			for (int i = 0; i < 3; i++) {
-				int next;
-				
-				if(i == 2) {
-					next = temp * dx[i];
-				} else {
-					next = temp + dx[i];
-				}
-				
-				if(next == K) {
-					System.out.println(chk[temp]);
-					return;
-				}
-				
-				if(next >= 0 && next < chk.length && chk[next] == 0) {
-					q.offer(next);
-					chk[next] = chk[temp] + 1;
-				}
-			}
-		}
-	}
 
+    static int N, K;
+    static int[] dx = {-1, 1, 2};
+    static int[] check = new int[100001];
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        System.out.println(bfs(N));
+    }
+
+    static int bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+
+        while (!q.isEmpty()) {
+            int current = q.poll();
+
+            if (current == K) {
+                return check[current];
+            }
+
+            for (int i = 0; i < 3; i++) {
+                int next;
+                if (i == 2) {
+                    next = current * dx[i];
+                } else {
+                    next = current + dx[i];
+                }
+
+                if (next >= 0 && next < 100001) {
+                    if (check[next] == 0) {
+                        check[next] = check[current]+1;
+                        q.offer(next);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
 }
